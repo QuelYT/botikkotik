db98a68aff79daa9572cb98fd36dd644c860401f
QuelYT <68297816+QuelYT@users.noreply.github.com>
2020-08-04 14:21:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 settings = {
 	'BOT NAME' : 'BotikKotik',
 	'ID' : '711626790052954225',
-	'PREFIX' : '$'
+	'PREFIX' : '$',
+	'JOIN LINK' : 'https://discord.com/api/oauth2/authorize?client_id=712951391726993470&permissions=8&scope=bot'
 }
